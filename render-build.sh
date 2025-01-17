60c13670e1122fef5e0a03ed699dcbe5802980c9
Purushothaman G <officialpurushothaman@gmail.com>
2025-01-17 15:58:42 +0530

    echo after install langs

--- render-build.sh
+++ render-build.sh
@@ -7,6 +7,9 @@ export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 export LANGUAGE="en_US:en"
 
+echo "locales after download..."
+locale -a
+
 # Proceed with other build steps
 pip install -r requirements.txt
 python app.py
