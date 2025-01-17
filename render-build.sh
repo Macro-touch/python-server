c87bd59d2ff8cf3cba3fca4848f2c7dc5fb90c8b
Purushothaman G <officialpurushothaman@gmail.com>
2025-01-17 15:19:23 +0530

    removed sudo

--- render-build.sh
+++ render-build.sh
@@ -1,19 +1,17 @@
-# Install locales
+# !/bin/bash
+# render-build.sh
+
+# Update and install required locales during the build process
 apt-get update && apt-get install -y locales
 
-mkdir -p ~/locales
-localedef --inputfile en_US --charmap UTF-8 ~/locales/en_US.UTF-8
-
 # Generate en_US.UTF-8 locale
-sudo locale-gen en_US.UTF-8
 locale-gen en_US.UTF-8
 
 # Set locale environment variables
-export LOCPATH=~/locales
 export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
-sudo dpkg-reconfigure locales
+export LANGUAGE=en_US:en
 
 echo "Locale setup complete: $(locale)"
-pip install -r requirements.txt
+
+echo "Available Locales: \n"
+locale -a
