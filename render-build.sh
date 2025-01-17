ec3112e13eb9b0a58fc46dd6eefcd242354a029f
Purushothaman G <officialpurushothaman@gmail.com>
2025-01-17 15:06:55 +0530

    render-build file updated

--- render-build.sh
+++ render-build.sh
@@ -2,12 +2,14 @@
 apt-get update && apt-get install -y locales
 
 # Generate en_US.UTF-8 locale
+sudo locale-gen en_US.UTF-8
 locale-gen en_US.UTF-8
 
 # Set locale environment variables
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US:en
 export LC_ALL=en_US.UTF-8
+export LC_CTYPE="en_US.UTF-8"
 sudo dpkg-reconfigure locales
 
 echo "Locale setup complete: $(locale)"
