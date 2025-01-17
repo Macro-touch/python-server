d84e09d0e5450e1c4d831d85ad0db5a871c687f2
Purushothaman G <officialpurushothaman@gmail.com>
2025-01-17 14:57:44 +0530

    render-build file updated

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+# Install locales
 apt-get update && apt-get install -y locales
+
+# Generate en_US.UTF-8 locale
 locale-gen en_US.UTF-8
+
+# Set locale environment variables
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US:en
 export LC_ALL=en_US.UTF-8
-python app.py
+
+echo "Locale setup complete: $(locale)"
+pip install -r requirements.txt
