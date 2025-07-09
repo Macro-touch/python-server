905dfb7e05399ba082d56e435bdaaac432dc79c8
Purushothaman G <officialpurushothaman@gmail.com>
2025-07-09 15:19:55 +0530

    fix(app): programmatic server startup revoked

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from routes import pdf_routes
-import uvicorn
-from multiprocessing import cpu_count
 
 app = FastAPI()
 
@@ -12,12 +10,3 @@ app.include_router(pdf_routes)
 @app.get("/")
 def root():
     return {"message": "FastAPI is running"}
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "app:app", 
-        host="127.0.0.1", 
-        port=8000, 
-        log_level="info", 
-        workers=cpu_count()
-    )
